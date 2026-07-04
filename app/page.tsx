6838b1c3e9f5e2686c8d2f3c6c07268715aa0864
josephW1805 <joseph.weng.1805@gmail.com>
2026-07-04 11:17:05 -0400

    step-1-shadcn-ui

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <ThemeToggle />
+    </div>
+  );
+}
