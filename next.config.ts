3152130616dd3db6ad901453c53f9684834c047e
josephW1805 <joseph.weng.1805@gmail.com>
2026-07-05 20:59:38 -0400

    feat: add channel management features
    - Implemented ChannelList component to display a list of channels.
    - Created CreateNewChannel component for adding new channels with validation.
    - Added WorkspaceHeader component to show the current workspace name.
    - Developed WorkspaceMembersList component to list workspace members.
    - Introduced layout for workspace pages, integrating channel and member lists.
    - Added routing for channel-related API endpoints, including listing and creating channels.
    - Defined channel schema with validation using Zod.
    - Set up Prisma client with Neon adapter for database interactions.
    - Configured Prisma schema for channel management.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatar.githubusercontent.com",
+        protocol: "https",
+      },
+      {
+        hostname: "*.googleusercontent.com",
+        protocol: "https",
+      },
+      {
+        hostname: "avatar.vercel.sh",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
